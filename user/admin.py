c82b62360efc3c2eaf2e2e7cc28402622e3e4a19
jerry <jerry@gmail.com>
2022-01-24 21:58:33 +0800

    user model create.

---
+++ user/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import City, Respondent, Profile
+# Register your models here.
+admin.site.register(City)
+admin.site.register(Respondent)
+admin.site.register(Profile)
