9252e1d41c9cf19f4dd330d944ec49f7dd3dc8b3
jerry <17app001@gmail.com>
2022-01-26 21:08:05 +0800

    1.完成case model 2.第一個views cases.html

--- case/admin.py
+++ case/admin.py
@@ -6,3 +6,5 @@ admin.site.register(Amount)
 admin.site.register(State)
 admin.site.register(Mode)
 admin.site.register(Period)
+admin.site.register(Case)
+
--- case/models.py
+++ case/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from user.models import Profile,Respondent
+from django.db.models.deletion import SET_NULL,CASCADE
 
 # Create your models here.
 class Category(models.Model):
@@ -51,3 +53,26 @@ class Period(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Case(models.Model):
+    owner=models.ForeignKey(Profile,on_delete=CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    contact = models.CharField(max_length=100)
+    skill = models.TextField(null=True, blank=True)
+    createdon = models.DateTimeField(auto_now_add=True)
+    view = models.IntegerField(default=0)
+
+    category = models.ForeignKey(Category, null=True,on_delete=SET_NULL)
+    amount = models.ForeignKey(Amount, null=True, on_delete=SET_NULL)
+    period = models.ForeignKey(Period, null=True, on_delete=SET_NULL)
+    state = models.ForeignKey(State, null=True, on_delete=SET_NULL)
+    respondent = models.ManyToManyField(Respondent)   
+    mode = models.ManyToManyField(Mode)
+
+    class Meta:
+        ordering = ['-createdon']
+
+    def __str__(self):
+        return self.title
