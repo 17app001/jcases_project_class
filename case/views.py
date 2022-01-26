4ad67f65beefcfa17384c8f788813f09fd89dc77
jerry <17app001@gmail.com>
2022-01-26 21:08:05 +0800

    1.完成case model 2.第一個views cases.html 3.完成到帳號密碼錯誤顯示

--- case/views.py
+++ case/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Case
+from user.models import Profile
 
 # Create your views here.
+
+
+def cases(request):
+    cases = Case.objects.all()   
+
+    return render(request, './case/cases.html', {'cases': cases})
--- user/views.py
+++ user/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
-from .models import City
+from django.shortcuts import redirect, render
+from django.contrib.auth import login, logout, authenticate
 # Create your views here.
+
+
+def user_logout(request):
+    if request.user.is_authenticated:
+        logout(request)
+
+    return redirect('login')
+
+
+def user_login(request):
+    username, password = '', ''
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        print(username, password)
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        if user:
+            login(request, user)  # request.user
+            return redirect('cases')
+
+        print('登入失敗!')
+
+    return render(request, './user/login.html', {'username': username, 'password': password})
