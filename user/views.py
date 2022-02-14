0efb4edd3d3d20bca8a434e2b34c1deaa789ee3b
jerry <17app001@gmail.com>
2022-02-14 19:12:06 +0800

    完成使用者帳密檢查

--- user/views.py
+++ user/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import login, logout, authenticate
+from .models import Profile
 # Create your views here.
 
 
@@ -11,7 +12,7 @@ def user_logout(request):
 
 
 def user_login(request):
-    username, password = '', ''
+    username, password,message = '', '',''
 
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -19,11 +20,17 @@ def user_login(request):
 
         print(username, password)
         user = authenticate(request, username=username, password=password)
-        print(user)
+        # print(user)
         if user:
             login(request, user)  # request.user
             return redirect('cases')
 
-        print('登入失敗!')
+        #帳號錯誤
+        if Profile.objects.filter(username=username).exists():
+            message='密碼錯誤'
+        else:
+            message='帳號錯誤'
 
-    return render(request, './user/login.html', {'username': username, 'password': password})
+
+    return render(request, './user/login.html', {'username': username, 'password': password,
+    'message':message})
