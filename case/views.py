616466ccb2ecc2b61969779c1a89813310f6dfd1
jerry <17app001@gmail.com>
2022-02-23 20:59:06 +0800

    chapter6 .完成到分頁

--- case/views.py
+++ case/views.py
@@ -1,8 +1,11 @@
+import django
 from django.shortcuts import redirect, render
-from .models import Case
+from .models import Case, Category
+from user.models import City
 from .forms import CreateCaseForm
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
+from django.db.models import Q
 # Create your views here.
 
 def case(request,id):
@@ -76,6 +79,38 @@ def create_case(request):
     return render(request,'./case/create-case.html',{'form':form})
 
 def cases(request):
-    cases = Case.objects.all()   
+    category_id,county_id,search=0,0,''
+    categorys=Category.objects.all() 
+    countys=City.objects.all()
 
-    return render(request, './case/cases.html', {'cases': cases})
+    if request.method=='GET':
+        cases = Case.objects.all()  
+
+    if request.method=='POST':
+        category_id=eval(request.POST.get('category')) if request.POST.get('category') else 0
+        county_id=eval(request.POST.get('county')) if request.POST.get('county') else 0      
+        search=request.POST.get('search')
+
+        category_q=Q(category_id=category_id)
+        county_q= Q(owner__city_id=county_id)
+        search_q=Q(title__contains=search) | Q(description__contains=search)
+
+        print(category_q,county_q,search_q)
+
+        if category_id and county_id:
+            cases=Case.objects.filter(category_q & county_q & search_q) if search else\
+                 Case.objects.filter(category_q & county_q)       
+        elif category_id:
+            cases=Case.objects.filter(category_q & search_q) if search else\
+                Case.objects.filter(category_q)
+        elif county_id:
+            cases=Case.objects.filter(county_q & search_q) if search else\
+                Case.objects.filter(county_q)
+        elif search:
+             cases=Case.objects.filter(search_q)        
+        else:
+            cases = Case.objects.all()       
+    
+    print(cases)
+    return render(request, './case/cases.html', {'cases': cases,'categorys':categorys,
+    'countys':countys,'category_id':category_id,'county_id':county_id,'search':search})
